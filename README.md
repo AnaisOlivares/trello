4973df6e280381a54ec050ae5154986d69e0de31
Your Name <you@example.com>
2017-11-20 04:33:53 -0500

    añadiendo estructura

---
+++ README.md
@@ -0,0 +1,6 @@
+# TRELLO
+## INTRODUCCIÓN
+>Este reto consiste en recrear una réplica de la página Trello.
+
+## EJEMPLO MODELO
+> [Trello](https://media.giphy.com/media/l1J9Ai0amYnS22ChW/giphy.gif)
--- js/app.js
+++ js/app.js
@@ -1,47 +1,50 @@
-
-window.addEventListener('load',function() {
-	var spanerList = document.getElementById('spaner');
-var containerBox = document.getElementById('container');
-var firstBox = document.getElementById('firstId');
-	//function add(){
-		spanerList.addEventListener('click', function(){
-			var inp = document.createElement('input');
-			var btn = document.createElement('button');
-			var spn = document.createElement('span');
-
-			btn.textContent = 'Añadir una lista';
-			spn.textContent = 'x';
-			containerBox.appendChild(inp);
-			containerBox.appendChild(btn);
-			containerBox.appendChild(spn);
-			containerBox.removeChild(spanerList);
-//agregando clases
+window.addEventListener('load', function() {
+  var spanerList = document.getElementById('spaner');
+  var containerBox = document.getElementById('container');
+  var firstBox = document.getElementById('firstId');
+  // var form = document.getElementById("form");
+  // form.style.display = 'none';
+  //function add(){
+  spanerList.addEventListener('click', function() {
+    var inp = document.createElement('input');
+    var btn = document.createElement('button');
+    var spn = document.createElement('span');
 
 
+    btn.textContent = 'Añadir una lista';
+    spn.textContent = 'x';
+    containerBox.appendChild(inp);
+    containerBox.appendChild(btn);
+    containerBox.appendChild(spn);
+    // containerBox.appendChild(div);
+    containerBox.removeChild(spanerList);
+    //agregando clases
+
+
+    containerBox.classList.add('divSquare');
+
+
+    //creando evento al boton
+    btn.addEventListener('click', function() {
+      var list = document.createElement('p');
+      list.textContent = inp.value;
+      inp.value = '';
+      containerBox.appendChild(list);
+      containerBox.removeChild(inp);
+      containerBox.removeChild(btn);
+      containerBox.removeChild(spn);
+      //brother debe ser display:inline-block, debo crear clases;
+      var brother = document.createElement('div');
+      brother.appendChild(inp);
+      brother.appendChild(btn);
+      brother.appendChild(spn);
+      firstBox.appendChild(brother);
 
 
 
-//creando evento al boton
-btn.addEventListener('click',function(){
-	var list = document.createElement('p');
-	list.textContent = inp.value;
-	inp.value = '';
-	containerBox.appendChild(list);
-	containerBox.removeChild(inp);
-	containerBox.removeChild(btn);
-	containerBox.removeChild(spn);
-//brother debe ser display:inline-block, debo crear clases;
-	var brother = document.createElement('div');
-	brother.appendChild(inp);
-	brother.appendChild(btn);
-	brother.appendChild(spn);
-	firstBox.appendChild(brother);
+    })
 
 
-
-})
-
-
-		})
-	//}
+  })
+  //}
 });
